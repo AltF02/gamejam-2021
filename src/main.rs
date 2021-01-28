980f3ffe67a2db914354468a939a87d3e2cd84c3
DankDumpster <staalkoper@gmail.com>
2021-01-28 01:42:06 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+mod setup;
+mod models;
+
+use bevy::prelude::*;
+
+struct SnakeHead;
+struct Materials {
+    head_material: Handle<ColorMaterial>,
+}
+
+fn main() {
+    App::build()
+        .add_startup_system(setup.system())
+        .add_startup_stage("game_setup", SystemStage::single(spawn_snake.system()))
+        .add_system(snake_movement.system())
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.insert_resource(Materials {
+        head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+    });
+}
+
+fn spawn_snake(commands: &mut Commands, materials: Res<Materials>) {
+    commands
+        .spawn(SpriteBundle {
+            material: materials.head_material.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        })
+        .with(SnakeHead);
+}
+
+fn snake_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
+) {
+    for mut transform in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::A) {
+            transform.translation.x -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::D) {
+            transform.translation.x += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::S) {
+            transform.translation.y -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::W) {
+            transform.translation.y += 2.;
+        }
+    }
+}
